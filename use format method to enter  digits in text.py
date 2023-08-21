27c8eb86a18e6039ba1127f381048bcd2028786a
Mian_Hamza <ha2982214@gmail.com>
2023-08-22 02:12:23 +0500

    Create use format method to enter  digits in text.py

---
+++ use format method to enter  digits in text.py
@@ -0,0 +1,4 @@
+total_amount=int(input('enter amount:'))
+quantity=int(input('enter quantity:'))
+price=int(input('enter price:'))
+print('i have {} rupees so that i can buy {} jackets for {} rupees:'.format(total_amount,quantity,price))
