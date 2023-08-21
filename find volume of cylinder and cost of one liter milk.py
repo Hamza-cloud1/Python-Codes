2a3a0f927969e56b3c2e94107d3ee90ac6ba4e95
Mian_Hamza <ha2982214@gmail.com>
2023-08-22 02:14:12 +0500

    Create find volume of cylinder and cost of one liter milk.py

---
+++ find volume of cylinder and cost of one liter milk.py
@@ -0,0 +1,8 @@
+radius=float(input('enter radius:'))
+height=float(input('enter height:'))
+volume=3.142*(radius**2)*height
+liter=volume*1000
+cost=liter*200
+print('volume of cylinder:',volume)
+print('total capacity:',liter)
+print('total cost',cost)
