5e3b1e08cc0bb54a7d2e0e496465dfbb74ab5b86
Mian_Hamza <ha2982214@gmail.com>
2023-08-27 11:22:01 +0500

    Create input angles and check triangle or not.py

---
+++ input angles and check triangle or not.py
@@ -0,0 +1,7 @@
+a=int(input('enter angle 1:'))
+b=int(input('enter angle 2:'))
+c=int(input('enter angle 3:'))
+if (a+b+c)==180 and a!=0 and b!=0 and c!=0:
+    print('yes, its can form triangle:')
+else:
+    print('not a triangle:')
