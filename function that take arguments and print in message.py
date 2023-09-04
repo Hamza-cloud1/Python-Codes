8f3a047fdc8f1c6526c51adb2b43a6d007b772dc
Mian_Hamza <ha2982214@gmail.com>
2023-09-04 11:42:08 +0500

    Create function that take arguments and print in message.py

---
+++ function that take arguments and print in message.py
@@ -0,0 +1,7 @@
+def bio_data(name,age,weight,education):
+    print('name of the person is: {},age of the person is: {},weight: {},education: {}'.format(name,age,weight,education))
+name=input('enter name: ')
+age=int(input('enter age: '))
+weight=int(input('enter weight: '))
+edu=input('enter education: ')
+bio_data(name,age,weight,edu)
